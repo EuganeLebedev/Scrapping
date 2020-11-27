a70d20e0330e9f6f385fe46489bcea2454d98152
Eugane Lebedev <adm1n.lebedev@ya.ru>
2020-11-27 16:34:57 +0300

    Бот для отправки сообщений

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
 import telebot
 from scrapping import get_data_from_url
+import time
+import json
 
 
 if __name__ == "__main__":
-    print(get_data_from_url())
+    while True:
+        with open("settings.json") as file:
+            settings = json.load(file)
+
+        bot_token = settings.get("bot_token")
+        channel = settings.get("channel")
+        print(bot_token, channel)
+
+
+        message = get_data_from_url()
+        bot = telebot.TeleBot(bot_token)
+        bot.send_message(channel, message)
+        time.sleep(60*60*12)
+
+
+
 
