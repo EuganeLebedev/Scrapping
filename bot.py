bfc457d152e3ca38d89fa141c844352a10cc6796
Eugane Lebedev <adm1n.lebedev@ya.ru>
2020-11-27 13:36:57 +0300

    Получение цен из запроса и добавление их в файл

---
+++ bot.py
@@ -0,0 +1,7 @@
+import telebot
+from scrapping import get_data_from_url
+
+
+if __name__ == "__main__":
+    print(get_data_from_url())
+
--- scrapping.py
+++ scrapping.py
@@ -3,28 +3,60 @@ import requests
 import time
 import json
 
-url = "https://catalog.onliner.by/robotcleaner/xiaomi/mirovamoprwhgl"
-headers = {
+
+def set_config(filename="data.json"):
+    with open("data.json") as file:
+        config = json.load(file)
+
+    config["old_price"] = float(config.get("old_price"))
+
+    if config["old_price"] == None:
+        config["old_price"] = 999999
+
+    return config
+
+
+def override_price(data, filename="data.json"):
+    with open("data.json", "w") as file:
+        json.dump(data, file)
+
+
+def get_data_from_url():
+    headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
-      }
+    }
 
-request_result = requests.get(url, headers=headers).text
+    config = set_config()
+    url = config.get("url")
+    old_price = config.get("old_price")
 
-soup = BeautifulSoup(request_result, "html5lib").findChildren("div", {"class": "offers-description__part offers-description__part_1"})
-#print(price_data).findChildren("span", {"class": "helpers_show_tablet"})
-subresult = soup[0].findChildren("div", {"class": "offers-description__price"})
-price = (subresult[0].findChildren("span", {"class": "helpers_hide_tablet"}))
-price_text = price[0].text
+    request_result = requests.get(url, headers=headers).text
+
+    soup = BeautifulSoup(request_result, "html5lib").findChildren("div",
+                                                                  {
+                                                                      "class": "offers-description__part "
+                                                                               "offers-description__part_1"})
+    subresult = soup[0].findChildren("div", {"class": "offers-description__price"})
+    price = (subresult[0].findChildren("span", {"class": "helpers_hide_tablet"}))
+    price_text = price[0].text
+
+    new_price = float(price_text.split(" ")[0].replace(",", "."))
+
+    if old_price > new_price:
+        config["old_price"] = new_price
+        override_price(config)
+        message = f"Цена уменьщилась {config['old_price']}"
+    elif old_price == new_price:
+
+        message = 'Цена не изменилась'
+    else:
+
+        config["old_price"] = new_price
+        override_price(config)
+        message = f"Цена увеличилась {config['old_price']}"
+
+    return message
 
 
-new_price=float(price_text.split(" ")[0].replace(",", "."))
-
-old_price = 1000
-if old_price <= new_price:
-    print('Less')
-else:
-        print('Bigger')
-
-
-with open("data.json") as file:
-    config = json.load(file)
+if __name__ == "__main__":
+    get_data_from_url()
